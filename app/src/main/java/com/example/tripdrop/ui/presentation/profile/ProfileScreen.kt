af0c8c0e2c6f5e6f14e75e1cf1192141cdcffabe
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-09-26 19:04:25 +0530

    fixing ProfileScreen.kt

--- app/src/main/java/com/example/tripdrop/ui/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/tripdrop/ui/presentation/profile/ProfileScreen.kt
@@ -33,9 +33,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -54,7 +51,6 @@ import com.example.tripdrop.ui.presentation.common.CommonImage
 import com.example.tripdrop.ui.presentation.common.DefaultPadding
 import com.example.tripdrop.ui.presentation.common.IconSize
 import com.example.tripdrop.ui.presentation.common.LargePadding
-import com.example.tripdrop.ui.presentation.common.LoaderScreen
 import com.example.tripdrop.ui.presentation.common.SmallSpacing
 
 @Composable
